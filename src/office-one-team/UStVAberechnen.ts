7f4af4c91e188b543fe3d40e2a92569915dfc2cf
Patrick Weiss <patrick.weiss@officeone.team>
2020-07-14 17:03:22 +0200

    client state mit UStVA Berechnung aktualsiert

--- src/office-one-team/UStVAberechnen.ts
+++ src/office-one-team/UStVAberechnen.ts
@@ -2,9 +2,18 @@ function UStVAberechnen(rootFolderId: string){
     let BM = new BusinessModel(rootFolderId);
     BM.kontoSummenAktualisieren();
     BM.save();
+
+    var result = {
+        serverFunction: ServerFunction.getNamedRangeData,
+        rangeName: "UStVAD",
+        namedRangeData: BM.getUStVATableCache().getData()
+      }
+      return JSON.stringify(result);
+   /* 
     var result = {
         serverFunction: ServerFunction.UStVAberechnen,
         UStVAD: BM.getUStVATableCache().getData(),
     }
       return JSON.stringify(result);
+      */
 }
